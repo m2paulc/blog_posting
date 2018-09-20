c62fc72b31bccee7e34852732db201ed38861d9b
Paul C <hyp3rsp8c3@gmail.com>
2018-09-20 06:03:01 +0000

    completed oauth and routes authentication

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const passport = require("passport");
 const app = express();
+
+//Load user model
+require("./models/User")
 
 //Load passport config
 require("./config/passport")(passport);
@@ -10,8 +15,37 @@ require("./config/passport")(passport);
 // Load Routes
 const auth = require("./routes/auth");
 
+// Load keys
+const keys = require("./config/keys");
+
+//Map global promises
+mongoose.Promise = global.Promise;
+//Mongoose connect
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 app.get('/', (req, res) => {
     res.send('Index Route Established');
+});
+
+//cookie parser middleware
+app.use(cookieParser());
+//express-session middleware
+app.use(session({
+    secret: 'dogs can talk',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set global variables
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
 });
 
 //Use Routes
